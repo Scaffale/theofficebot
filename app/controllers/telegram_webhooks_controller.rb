94570b82629835e8367cc39ddd5db17c3ee8cf12
martinogiacchetti <Scaffale>
2019-02-22 14:29:29 +0100

    added seeds and edited main search

--- app/controllers/telegram_webhooks_controller.rb
+++ app/controllers/telegram_webhooks_controller.rb
@@ -11,7 +11,7 @@ class TelegramWebhooksController < Telegram::Bot::UpdatesController
     print "offset"
     print _offset
     # query = query.first(10) # it's just an example, don't use large queries.
-
+    max_results = 3
     query.strip!
     if query.blank?
       return
@@ -24,72 +24,34 @@ class TelegramWebhooksController < Telegram::Bot::UpdatesController
     print "Cerco: " + query
     print "Tempo Before: #{beforeTime}"
     print "Tempo After: #{afterTime}"
-    print 'Cerco nei file .srt'
-    frase = query
+    frase = query.split(' ')
     name = 0
-    files_srt.each do |file|
-      # print "Cerco in " + file
-      openedFile = open(file).readlines()
-      currentSentence = ""
-      startTime = ''
-      endTime = ''
-      openedFile.each_with_index do |line, index|
-        break if name > 3
-        line = line.strip
-        if is_sentence(line)
-          if is_begin(line)
-            currentSentence = line
-            # startTime
-            i = -1
-            while !is_time(openedFile[index + i].strip) do
-              i -= 1
-            end
-            startTime = get_time(openedFile[index + i].strip)[0]
-          else
-            currentSentence += line
-          end
-          if is_end(line)
-            # endTime
-            i = -1
-            while !is_time(openedFile[index + i].strip) do
-              i -= 1
-            end
-            endTime = get_time(openedFile[index + i].strip)[1]
-            # c'è un a capo
-            founded = true
-            frase.split(' ').each do |word|
-              unless currentSentence.downcase.include? word.downcase
-                founded = false
-              end
-            end
-            if founded
-              name += 1
-              print '~~~TROVATA~~~'
-              new_name = query.split(' ').sort().join('-') + name.to_s
-              start = get_seconds_from_time(startTime) - beforeTime
-              endTs = get_seconds_from_time(endTime) + afterTime
-              # Salvare il file con ffmpeg
-              # Darlo come url assoluto (panico)
-              comand = "ffmpeg -loglevel panic -ss #{start} -strict -2 -to #{endTs} -n -i #{file.gsub '.srt', '.mp4'} #{Rails.public_path}/gifs/#{new_name}.mp4"
-              print(comand)
-              # system(comand)
-              fork { exec(comand) }
-              lt_url = "https://average-snake-30.localtunnel.me"
-              p "#{lt_url}/gifs/#{new_name}.mp4"
-              results << {
-                type: :mpeg4_gif,
-                id: "#{new_name}",
-                mpeg4_url: "#{lt_url}/gifs/#{new_name}.mp4",
-                thumb_url: "#{lt_url}/placeholder.jpg"
-              }
-              #  ffmpeg -ss 00:01:00 -i input.mp4 -to 00:02:00 -c copy output.mp4
-              # -strict -2 probabilmente non è istantaneo come -c copy, ma taglia correttamente
-            end
-          end
-        end
-      end
+    lt_url = "https://breezy-wolverine-5.localtunnel.me"
+
+    finded_sentences = Sentence.where(frase.map{|e| "(lower(text) LIKE '%#{e.downcase}%')"}.join(' AND ')).limit(max_results)
+
+    finded_sentences.each do |sentence|
+      p sentence
     end
-    # rispondere subito poi create le gif in thread separato
+    finded_sentences.each do |sentence|
+      start = sentence.start_time - beforeTime
+      endTs = sentence.end_time + afterTime
+      name += 1
+      new_name = frase.sort().join('-') + name.to_s
+      comand = "ffmpeg -ss #{start} -loglevel panic -to #{endTs} -n -i #{Rails.root}/data/srt/#{sentence.file_name.gsub '.srt', '.mp4'} -vcodec copy -copyinkf #{Rails.public_path}/gifs/#{new_name}.mp4"
+      # comand = "ffmpeg -loglevel panic -ss #{start} -strict -2 -to #{endTs} -n -i #{Rails.root}/data/srt/#{sentence.file_name.gsub '.srt', '.mp4'} #{Rails.public_path}/gifs/#{new_name}.mp4"
+      print(comand)
+      system(comand)
+      # fork { exec(comand) }
+      p "#{lt_url}/gifs/#{new_name}.mp4"
+      results << {
+        type: :mpeg4_gif,
+        id: "#{new_name}",
+        mpeg4_url: "#{lt_url}/gifs/#{new_name}.mp4",
+        thumb_url: "#{lt_url}/placeholder.jpg"
+      }
+    end
+    # rispondere subito poi create le gif in thread separato ?
     answer_inline_query results
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,66 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+include TimeHelper
+
+Sentence.delete_all
+
+currentSentence = ""
+startTime = ''
+endTime = ''
+current_filename = ''
+season = 0
+episode = 0
+Dir.glob("#{Rails.root}/data/srt/*.srt").each do |file|
+  # Ultima frase del file precente
+  if currentSentence != ""
+    start = get_seconds_from_time(startTime)
+    endTs = get_seconds_from_time(endTime)
+    Sentence.create!(season: season, 
+                     episode: episode, 
+                     file_name: current_filename, 
+                     end_time: endTs, 
+                     start_time: start,
+                     text: currentSentence)
+  end
+  current_filename = file.split('/')[-1]
+  season = current_filename.split('E')[0].split('S')[1].to_i
+  episode = current_filename.split('_')[0].split('E')[1].to_i
+  print "Analizzo " + file
+  openedFile = open(file).readlines()
+  is_not_ended = true
+  openedFile.each_with_index do |line, index|
+    line = line.strip
+    if is_sentence(line)
+      if is_begin(line) && is_not_ended
+        is_not_ended = false
+        currentSentence = line
+        # startTime
+        i = -1
+        while !is_time(openedFile[index + i].strip) do
+          i -= 1
+        end
+        startTime = get_time(openedFile[index + i].strip)[0]
+      else
+        currentSentence += " #{line}"
+      end
+      # endTime
+      i = -1
+      while !is_time(openedFile[index + i].strip) do
+        i -= 1
+      end
+      endTime = get_time(openedFile[index + i].strip)[1]
+      if is_end(line)
+        is_not_ended = true
+        start = get_seconds_from_time(startTime)
+        endTs = get_seconds_from_time(endTime)
+        Sentence.create!(season: season, 
+                         episode: episode, 
+                         file_name: current_filename, 
+                         end_time: endTs, 
+                         start_time: start,
+                         text: currentSentence)
+      end
+    end
+  end
+end
